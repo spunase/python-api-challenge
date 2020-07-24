4d371c8b99cf64802583c63bdbf10074199505f2
CodingGirl <spunase@gmail.com>
2020-07-23 23:09:45 -0500

    final

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "6b8d9c79f06fd174cf0280c88b3f4a09"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyD9qXkaDyc4ekN29eCjqQYOXi7NZhlHSYo"
